ef9691462bf973a8ba4158b6c0e758e9a3e83fe9
sekercih <hkkskrc@gmail.com>
2020-09-23 19:54:22 +0300

    Cucumberfreamwork

--- src/test/java/runners/GoogleRunner.java
+++ src/test/java/runners/GoogleRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "stepdefinitions",
         tags = "@amazonurunarama", //tags ="@feature" tümünü çalıştırır, {"@techpro or @selenium"},
-        dryRun = false
+        dryRun = true
 
 
 )
